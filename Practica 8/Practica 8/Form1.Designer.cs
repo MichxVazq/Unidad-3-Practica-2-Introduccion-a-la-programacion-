4b0ee5be21139fcb95f73902794b08be05312eed
MichxVazq <l25580441@reynosa.tecnm.mx>
2025-10-26 22:41:58 -0500

    Add files via upload

---
+++ Practica 8/Practica 8/Form1.Designer.cs
@@ -0,0 +1,90 @@
+﻿namespace Practica_8
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            label1 = new Label();
+            listBox1 = new ListBox();
+            button1 = new Button();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI", 26.25F, FontStyle.Bold | FontStyle.Italic, GraphicsUnit.Point, 0);
+            label1.Location = new Point(138, 29);
+            label1.Name = "label1";
+            label1.Size = new Size(530, 47);
+            label1.TabIndex = 0;
+            label1.Text = "Este progama escribe del 1 al 5";
+            // 
+            // listBox1
+            // 
+            listBox1.BackColor = Color.FromArgb(255, 192, 192);
+            listBox1.Font = new Font("Segoe UI", 24F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            listBox1.FormattingEnabled = true;
+            listBox1.ItemHeight = 45;
+            listBox1.Location = new Point(156, 120);
+            listBox1.Name = "listBox1";
+            listBox1.Size = new Size(344, 274);
+            listBox1.TabIndex = 1;
+            // 
+            // button1
+            // 
+            button1.BackColor = Color.Red;
+            button1.Font = new Font("Segoe UI", 18F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            button1.Location = new Point(541, 231);
+            button1.Name = "button1";
+            button1.Size = new Size(222, 60);
+            button1.TabIndex = 2;
+            button1.Text = "Listar números";
+            button1.UseVisualStyleBackColor = false;
+            button1.Click += button1_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.FromArgb(255, 192, 128);
+            ClientSize = new Size(800, 450);
+            Controls.Add(button1);
+            Controls.Add(listBox1);
+            Controls.Add(label1);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private ListBox listBox1;
+        private Button button1;
+    }
+}
