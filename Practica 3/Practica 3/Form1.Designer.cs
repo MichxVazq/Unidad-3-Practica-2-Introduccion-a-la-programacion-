18bfe54c5b842fc1904842c872a1c629fa68f3e1
MichxVazq <l25580441@reynosa.tecnm.mx>
2025-10-26 22:40:21 -0500

    Add files via upload

---
+++ Practica 3/Practica 3/Form1.Designer.cs
@@ -0,0 +1,152 @@
+﻿namespace Practica_3
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            label1 = new Label();
+            label2 = new Label();
+            textBox1 = new TextBox();
+            button1 = new Button();
+            label3 = new Label();
+            textBox2 = new TextBox();
+            button2 = new Button();
+            listBox1 = new ListBox();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI", 21.75F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label1.Location = new Point(78, 20);
+            label1.Name = "label1";
+            label1.Size = new Size(638, 40);
+            label1.TabIndex = 0;
+            label1.Text = "Este programa suma los numeros capturados";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Segoe UI", 18F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label2.Location = new Point(43, 116);
+            label2.Name = "label2";
+            label2.Size = new Size(264, 32);
+            label2.TabIndex = 1;
+            label2.Text = "Introduce un Numero";
+            // 
+            // textBox1
+            // 
+            textBox1.Font = new Font("Segoe UI", 18F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            textBox1.Location = new Point(313, 113);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(194, 39);
+            textBox1.TabIndex = 2;
+            // 
+            // button1
+            // 
+            button1.BackColor = Color.FromArgb(255, 128, 128);
+            button1.Font = new Font("Segoe UI", 18F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            button1.Location = new Point(313, 158);
+            button1.Name = "button1";
+            button1.Size = new Size(194, 47);
+            button1.TabIndex = 3;
+            button1.Text = "Añadir";
+            button1.UseVisualStyleBackColor = false;
+            button1.Click += button1_Click;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("Segoe UI", 18F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label3.Location = new Point(140, 318);
+            label3.Name = "label3";
+            label3.Size = new Size(269, 32);
+            label3.TabIndex = 4;
+            label3.Text = "La suma de todos son:";
+            // 
+            // textBox2
+            // 
+            textBox2.Font = new Font("Segoe UI", 18F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            textBox2.Location = new Point(415, 318);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(194, 39);
+            textBox2.TabIndex = 5;
+            // 
+            // button2
+            // 
+            button2.BackColor = Color.FromArgb(255, 128, 128);
+            button2.Font = new Font("Segoe UI", 18F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            button2.Location = new Point(11, 295);
+            button2.Name = "button2";
+            button2.Size = new Size(123, 85);
+            button2.TabIndex = 6;
+            button2.Text = "Sumar Numeros";
+            button2.UseVisualStyleBackColor = false;
+            button2.Click += button2_Click;
+            // 
+            // listBox1
+            // 
+            listBox1.FormattingEnabled = true;
+            listBox1.ItemHeight = 15;
+            listBox1.Location = new Point(616, 114);
+            listBox1.Name = "listBox1";
+            listBox1.Size = new Size(179, 319);
+            listBox1.TabIndex = 7;
+            listBox1.SelectedIndexChanged += listBox1_SelectedIndexChanged;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(listBox1);
+            Controls.Add(button2);
+            Controls.Add(textBox2);
+            Controls.Add(label3);
+            Controls.Add(button1);
+            Controls.Add(textBox1);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Name = "Form1";
+            Text = "Form1";
+            Load += Form1_Load;
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private Label label2;
+        private TextBox textBox1;
+        private Button button1;
+        private Label label3;
+        private TextBox textBox2;
+        private Button button2;
+        private ListBox listBox1;
+    }
+}
