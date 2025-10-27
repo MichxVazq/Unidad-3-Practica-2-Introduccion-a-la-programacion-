04f1e48a10c49d0718c6f3c7ba5b96af6d8e9d41
MichxVazq <l25580441@reynosa.tecnm.mx>
2025-10-26 22:39:59 -0500

    Add files via upload

---
+++ Practica 2/Practica 2/Form1.Designer.cs
@@ -0,0 +1,165 @@
+﻿namespace Practica_2
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            label1 = new Label();
+            label2 = new Label();
+            label3 = new Label();
+            label4 = new Label();
+            label5 = new Label();
+            textBox1 = new TextBox();
+            textBox2 = new TextBox();
+            listBox1 = new ListBox();
+            button1 = new Button();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI", 26.25F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label1.Location = new Point(185, 42);
+            label1.Name = "label1";
+            label1.Size = new Size(405, 47);
+            label1.TabIndex = 0;
+            label1.Text = "Tabla de Multiplicación";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Segoe UI", 18F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label2.Location = new Point(12, 114);
+            label2.Name = "label2";
+            label2.Size = new Size(218, 32);
+            label2.TabIndex = 1;
+            label2.Text = "Introduce el valor";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("Segoe UI", 18F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label3.Location = new Point(12, 146);
+            label3.Name = "label3";
+            label3.Size = new Size(303, 32);
+            label3.TabIndex = 2;
+            label3.Text = "de la Tabla de Multiplicar";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Font = new Font("Segoe UI", 18F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label4.Location = new Point(520, 114);
+            label4.Name = "label4";
+            label4.Size = new Size(268, 32);
+            label4.TabIndex = 3;
+            label4.Text = "Limite maximo para la";
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Font = new Font("Segoe UI", 18F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label5.Location = new Point(520, 146);
+            label5.Name = "label5";
+            label5.Size = new Size(276, 32);
+            label5.TabIndex = 4;
+            label5.Text = "tabla de multiplicacion";
+            // 
+            // textBox1
+            // 
+            textBox1.BackColor = SystemColors.Info;
+            textBox1.Font = new Font("Segoe UI", 15.75F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            textBox1.Location = new Point(75, 181);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(145, 35);
+            textBox1.TabIndex = 5;
+            // 
+            // textBox2
+            // 
+            textBox2.BackColor = SystemColors.Info;
+            textBox2.Font = new Font("Segoe UI", 15.75F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            textBox2.Location = new Point(590, 181);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(145, 35);
+            textBox2.TabIndex = 6;
+            // 
+            // listBox1
+            // 
+            listBox1.BackColor = Color.FromArgb(255, 192, 128);
+            listBox1.FormattingEnabled = true;
+            listBox1.ItemHeight = 15;
+            listBox1.Location = new Point(259, 181);
+            listBox1.Name = "listBox1";
+            listBox1.Size = new Size(275, 259);
+            listBox1.TabIndex = 7;
+            // 
+            // button1
+            // 
+            button1.BackColor = Color.FromArgb(255, 128, 128);
+            button1.Font = new Font("Segoe UI", 18F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            button1.Location = new Point(590, 397);
+            button1.Name = "button1";
+            button1.Size = new Size(150, 43);
+            button1.TabIndex = 8;
+            button1.Text = "Calcular";
+            button1.UseVisualStyleBackColor = false;
+            button1.Click += button1_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = SystemColors.ActiveBorder;
+            ClientSize = new Size(800, 450);
+            Controls.Add(button1);
+            Controls.Add(listBox1);
+            Controls.Add(textBox2);
+            Controls.Add(textBox1);
+            Controls.Add(label5);
+            Controls.Add(label4);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private Label label4;
+        private Label label5;
+        private TextBox textBox1;
+        private TextBox textBox2;
+        private ListBox listBox1;
+        private Button button1;
+    }
+}
